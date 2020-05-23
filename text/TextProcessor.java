2dd5fc536a0ac32e04955cbfcc28f5d72f77f6af
Aleksey Sverbeev <aleksey_sverbeev@epam.com>
2020-05-24 01:44:41 +0300

    age group refactor

--- text/TextProcessor.java
+++ text/TextProcessor.java
@@ -14,36 +14,14 @@ public class TextProcessor {
     }
 
     public static int getAgeGroup(double score) {
-        switch ((int) Math.ceil(score)) {
-            case 1:
-                return 6;
-            case 2:
-                return 7;
-            case 3:
-                return 9;
-            case 4:
-                return 10;
-            case 5:
-                return 11;
-            case 6:
-                return 12;
-            case 7:
-                return 13;
-            case 8:
-                return 14;
-            case 9:
-                return 15;
-            case 10:
-                return 16;
-            case 11:
-                return 17;
-            case 12:
-                return 18;
-            case 13:
-            case 14:
-                return 24;
-            default:
-                return 5;
+        int[] values = new int[]{6, 7, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 24};
+        int ind = (int) Math.ceil(score) - 1;
+        if (ind < 0) {
+            ind = 1;
         }
+        if (ind > values.length-1) {
+            ind = values.length-1;
+        }
+        return values[ind];
     }
 }
